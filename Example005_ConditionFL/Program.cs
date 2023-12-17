1e13cd71228702ac9dbb19a473ba6b23b22c0d35
Alena <alena.supernatural666@gmail.com>
2023-12-17 21:57:58 +0300

    Example 005 ConditionFL

---
+++ Example005_ConditionFL/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username == "серёжа")
+{
+    Console.WriteLine("Ура, это же Серёжа!");
+} 
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
